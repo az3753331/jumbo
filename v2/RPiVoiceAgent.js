b782d5821fcc5a1ab61bfbbf2b86349f78850c08
Michael Chi <michi@microsoft.com>
2017-04-07 16:46:14 +0800

    add speaker capability to voice agent

--- v2/RPiVoiceAgent.js
+++ v2/RPiVoiceAgent.js
@@ -1,5 +1,7 @@
 var events = require('events');
 var util = require('util');
+var JUMBO = require('./botclient.js');
+var stream = require('stream');
 const record = require('node-record-lpcm16');
 const Detector = require('snowboy').Detector;
 const Models = require('snowboy').Models;
@@ -10,6 +12,7 @@ const BOTCLIENT = require('./botclient.js');
 const HTTPSWAPPER = require('./https-helper.js'); 
 const URL = require('url');
 const models = new Models();
+var Speaker = require('speaker');
 
 const AUDIO_CONFIG = {
     sampleRate: 16000,
@@ -25,16 +28,27 @@ var modelOptionsDefaults = {
   sensitivity: '0.5',
   hotwords : 'hello'
 };
+var ERROR_WAV = {
+                            ERROR_ERROR_OCCUR : './media/erroroccur.wav',
+                            ERROR_IDONTUNDERSTAND : './media/idontunderstand.wav'
+                };
+var bot = null;
 
-function RPiVoiceAgent (storageInfo, modelOptions) { 
+function RPiVoiceAgent (storageInfo, modelOptions, botInfo , errors ) { 
     STORAGE_INFO = storageInfo;
     blobSvc = azure.createBlobService(storageInfo.account,storageInfo.key);
+    ERROR_WAV = Object.assign(ERROR_WAV, errors);
     modelOptions = Object.assign(modelOptionsDefaults,modelOptions);
     models.add(modelOptions);  
 
+    bot = new JUMBO(botInfo.botId,botInfo.userId,storageInfo);
+    bot.setDirectlineSecret(botInfo.DirectLineSecret);
     events.EventEmitter.call(this);
 }
+
 util.inherits(RPiVoiceAgent, events.EventEmitter);
+
+
 
 function _stopListening(){
     record.stop();
@@ -62,18 +76,32 @@ function _startListeningAndUpload(onEnd){
                                                         }
                                                     });
     ws.on('close', function(){
-        onVoiceUploadedHandler(STORAGE_INFO.account,STORAGE_INFO.container,fn);  
-    });
-    m.on('data', function(data){
-        console.log('...speaking...');
-    });
-    m.on('end',function(){
-        console.log('...end...');
-        _stopListening();
-        onEnd(m);
-    });
+        var result = onVoiceUploadedHandler(STORAGE_INFO.account,STORAGE_INFO.container,fn);
+        if(result == null || result == 'undefined')
+        {
+            console.log('sending result:' + JSON.stringify(result));
+            result = [{
+                        contentType:'audio/wav',
+                        contentUrl:'https://' + STORAGE_INFO.account + '.blob.core.windows.net/' +
+                                                STORAGE_INFO.container + '/' +
+                                                fn,
+                        name:fn
+                    }];
+        }
+        bot.sendMessage(
+                    '',
+                    result);        
+            });
+            m.on('data', function(data){
+                console.log('...speaking...');
+            });
+            m.on('end',function(){
+                console.log('...end...');
+                _stopListening();
+                onEnd(m);
+            });
 
-    m.pipe(ws);
+            m.pipe(ws);
 }
 
 function _start(){
@@ -98,7 +126,8 @@ function _start(){
         _startListeningAndUpload(
             function (listeningMic){
                 //module.exports.start();
-                _start();
+                console.log('_startListeningAndUpload callback called');
+                //_start();
             });
     });
     var m = record.start({
@@ -114,13 +143,72 @@ function _start(){
     m.pipe(detector);
 }
 
-var onVoiceUploadedHandler = null,
-    onHotwordDetectedHandler = null;
 
-RPiVoiceAgent.prototype.start = function(onVoiceUploaded,onHotwordDetected){
+var onVoiceUploadedHandler = null,
+    onHotwordDetectedHandler = null,
+    onBotReplyReceivedHandler = null;
+
+RPiVoiceAgent.prototype.start = function(onVoiceUploaded,onHotwordDetected,onBotReplyReceived){
     onVoiceUploadedHandler = onVoiceUploaded;
+    onBotReplyReceivedHandler = onBotReplyReceived;
     onHotwordDetectedHandler = onHotwordDetected;
+
+
+    bot.accquireToken(
+        function(data){
+            var result = bot.startConversation(function(data){
+                onBotReplyReceivedHandler(data);
+                console.log('****** message recevied from bot = ' + data.text);
+                if(data.attachments != null && data.attachments != 'undefined'){
+                    
+                    var url = data.attachments[0].contentUrl;
+                    console.log('has attachments:' + url);
+                    //Streaming
+                    if(true){
+                        var speaker = new Speaker({
+                            channels: 1,          // 2 channels
+                            bitDepth: 16,         // 16-bit samples
+                            sampleRate: 16000     // 44,100 Hz sample rate
+                        });
+                        speaker.on('error',function(err){
+                            console.log('speaker error=' + err);
+                            //TODO: play a local wav file to notify error
+                            _start();
+                        });
+                        speaker.on('close',function(){
+                            console.log('speaker closed');
+                            _start();
+                        });
+                        console.log('has attachments, playing audio...' + STORAGE_INFO.container + '/' + data.attachments[0].name);
+                        var passStream = bot.getDownloadStream(
+                                                STORAGE_INFO.container, 
+                                                data.attachments[0].name, 
+                                                function(err,result,response){
+                                                    console.log('in stream download completed event, closing passthrough stream...');
+                                                    passStream.end();
+                                            });
+                        //we will need to pause hotwords detection in order to prevent unexpected sound detect
+                        passStream.pipe(speaker);
+                    }else{
+                        //file download
+                        bot.downloadFile(data.attachments[0].name, data.attachments[0].name,
+                            function (error, result, response){
+                                if(!error){
+                                    
+                                }
+                        });                        
+                    }
+                }
+            });
+            convId = result.conversationId;
+        },
+        function(error){
+            console.log('error=' + error);
+        }
+    );
+
     _start();
+    
 }
 
 //_setupHotwordDetection();
--- v2/testrpi.js
+++ v2/testrpi.js
@@ -1,6 +1,4 @@
 var RPI = require('./RPiVoiceAgent.js');
-var JUMBO = require('./botclient.js');
-var stream = require('stream');
 
 const AUDIO_CONFIG = 
 {
@@ -16,6 +14,8 @@ const CONFIGURATION =
                     container:'upload'
                 },
     BotInfo:{
+        botId:'jumbo',
+        userId:'michael',
         DirectLineSecret:'X-O-skejDE0.cwA.n0k.PX7roOwYzPtkqr50ClLjRRBhz3v0e0rIYLgz7fXZjL4'
     },
     CognitiveServiceInfo:{
@@ -23,65 +23,31 @@ const CONFIGURATION =
     }
 };
 
-var Speaker = require('speaker');
 
-var bot = new JUMBO('jumbo','michael',CONFIGURATION.StorageInfo);
-bot.setDirectlineSecret(CONFIGURATION.BotInfo.DirectLineSecret);
-bot.accquireToken(function(data){
-        var result = bot.startConversation(function(data){
-            console.log('****** message recevied from bot = ' + data.text);
-            var id = data.id.split("|")[0];
-            var wavFN = id + '-sync.wav';
-            console.log('==>>' + data.attachments != null && data.attachments != 'undefined');
-            if(data.attachments != null && data.attachments != 'undefined'){
-                var url = data.attachments[0].contentUrl;
-                //Streaming
-                var speaker = new Speaker({
-                    channels: 1,          // 2 channels
-                    bitDepth: 16,         // 16-bit samples
-                    sampleRate: 16000     // 44,100 Hz sample rate
-                });
-                speaker.on('error',function(err){
-                    console.log('err=' + err);
-                });
-                var passStream = bot.getDownloadStream(
-                            CONFIGURATION.StorageInfo.container, 
-                            data.attachments[0].name, 
-                            function(err,result,response){
-                                passStream.end();
-                            });
-                passStream.pipe(speaker);
-            }
-        });
-        convId = result.conversationId;
-    },
-    function(error){
-        console.log('error=' + error);
-    }
-); 
 
 var agent = new RPI(CONFIGURATION.StorageInfo,
                         {
-                        file: 'resources/hello2.pmdl',
-                        sensitivity: '0.5',
-                        hotwords : 'hello'
-                    });
+                            file: 'resources/hello2.pmdl',
+                            sensitivity: '0.5',
+                            hotwords : 'hello'
+                        },
+                        CONFIGURATION.BotInfo,
+                        {
+                            ERROR_ERROR_OCCUR : './media/erroroccur.wav',
+                            ERROR_IDONTUNDERSTAND : './media/idontunderstand.wav'
+                        });
 agent.start(
     /* voice uploaded */
     function(account,container,fn){
         //send message to jumbo
-        bot.sendMessage(
-            '',
-            [{
-                contentType:'audio/wav',
-                contentUrl:'https://' + account + '.blob.core.windows.net/' +
-                                        container + '/' +
-                                        fn,
-                name:fn
-            }]);
+        return null;
     },
     /* hotword detected */
     function(){
 
+    },
+    /* bot replied */
+    function(data){
+
     }
-);;
+);
